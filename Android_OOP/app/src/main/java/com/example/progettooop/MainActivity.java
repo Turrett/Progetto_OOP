af7cc73c58729365a651ff69fc7b919d10c1107d
Turrett <45496975+Turrett@users.noreply.github.com>
2020-07-29 16:19:13 +0200

    Revert "mpods"
    This reverts commit 3d5c0c1b8ef9710d37646609854d5e1d7ea30462, reversing
    changes made to df89aea32801fb6903df9549ac74c9fdf2375caa.

--- Android_OOP/app/src/main/java/com/example/progettooop/MainActivity.java
+++ Android_OOP/app/src/main/java/com/example/progettooop/MainActivity.java
@@ -1,32 +1,33 @@
 package com.example.progettooop;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
+import com.example.progettooop.ui.home.HomeViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        setContentView(R.layout.fragment_main);
+        NavController navController;
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.navigation_search)
-                .build();
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.navigation_search, R.id.navigation_user)
+                .build();BottomNavigationView navView = findViewById(R.id.nav_view);
 
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
-
 }
