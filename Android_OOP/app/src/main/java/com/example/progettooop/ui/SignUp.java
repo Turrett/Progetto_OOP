5d55c8cbe656f532da9701cf2c9563d4530c9b0c
Turrett <45496975+Turrett@users.noreply.github.com>
2020-08-03 14:23:13 +0200

    boh

--- Android_OOP/app/src/main/java/com/example/progettooop/ui/SignUp.java
+++ Android_OOP/app/src/main/java/com/example/progettooop/ui/SignUp.java
@@ -12,7 +12,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.example.progettooop.ui.FirebaseUtil;
 
 import com.example.progettooop.R;
 
@@ -22,7 +21,6 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.*;
-import com.google.firebase.database.*;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
--- Android_OOP/app/src/main/java/com/example/progettooop/ui/user/UserFragment.java
+++ Android_OOP/app/src/main/java/com/example/progettooop/ui/user/UserFragment.java
@@ -5,15 +5,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.progettooop.R;
+import com.google.firebase.firestore.*;
+import com.google.firebase.auth.*;
+
+import java.util.Objects;
 
 
 public class UserFragment extends Fragment implements View.OnClickListener {
 
+    private TextView username;
     private UserViewModel userViewModel;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
@@ -22,12 +33,27 @@ public class UserFragment extends Fragment implements View.OnClickListener {
         return root;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        username.findViewById(R.id.user_username);
 
+
+    }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.modify_button);
         startActivity(new Intent(getContext(), ModifyUserInfo.class));
+    }
+
+    private void getInfo(){
+        DocumentReference doc = db.collection("utenti")
+                .document(Objects.requireNonNull(mAuth.getCurrentUser()).getUid())
+                .
+        username.setText();
     }
 }
 
