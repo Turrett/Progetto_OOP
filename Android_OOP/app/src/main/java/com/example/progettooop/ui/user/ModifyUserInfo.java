fa7b27894ed6f5eeeb0f2112d1781d5391978263
giovannaferraro <68597201+giovannaferraro@users.noreply.github.com>
2020-08-03 14:04:01 +0200

    SI PU' FAREEEEEEEE - add image by imageview - dobbiamo solo correggere onsuccess perché entra in conflitto con l'aggiunta di user, address e telephone number

--- Android_OOP/app/src/main/java/com/example/progettooop/ui/user/ModifyUserInfo.java
+++ Android_OOP/app/src/main/java/com/example/progettooop/ui/user/ModifyUserInfo.java
@@ -1,20 +1,27 @@
 package com.example.progettooop.ui.user;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.progettooop.R;
@@ -31,11 +38,16 @@ import com.google.firebase.firestore.*;
 import com.google.firebase.storage.*;
 import com.karumi.dexter.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+
+import io.grpc.Context;
 
 import static android.content.ContentValues.TAG;
 
@@ -46,16 +58,16 @@ public class ModifyUserInfo extends AppCompatActivity implements View.OnClickLis
 
     private final static String TAG = "Upload user";
     private ImageView image;
-    private EditText username, address, phone;
+    private EditText username, address, phone, imageName;
     private Button save;
     private final static int mWidth = 30;
     private final static int mLenght = 30;
-
     private ArrayList<String> pathArray; //verrà salvato il percorso dell'immagine
     private int array_position;
     private StorageReference mStorageRef;
     private FirebaseAuth mAuth;
     public FirebaseFirestore db;
+    public Uri imguri;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -70,61 +82,133 @@ public class ModifyUserInfo extends AppCompatActivity implements View.OnClickLis
         pathArray = new ArrayList<>();
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
-        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference("immagini");
+        image.setClickable(true);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileChooser();
+            }
+        });
 
-        checkFilePermissions();
-
-
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FileUploader();
+            }
+        });
 
 
     }
 
+    private String getExtension(Uri uri){
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimetypemap = MimeTypeMap.getSingleton();
+        return mimetypemap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void FileUploader(){
+        StorageReference  ref = mStorageRef.child(System.currentTimeMillis()+"."+getExtension(imguri));
+
+        ref.putFile(imguri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(ModifyUserInfo.this, "image uploaded corectlly", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void checkFilePermissions() {
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
-            int permissionCheck = ModifyUserInfo.this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
-            permissionCheck += ModifyUserInfo.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
-            if (permissionCheck != 0) {
-                this.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1001); //Any number
-            }
-        }else{
-            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+    private void FileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/jpeg");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data!= null && data.getData()!=null) {
+            imguri = data.getData();
+            image.setImageURI(imguri);
         }
     }
 
 
 
+    private void saveUserInfo() {
+        String nick = username.getText().toString();
+        if (nick.isEmpty()) {
+            username.setError("Name Required");
+            username.requestFocus();
+            return;
+        }
+
+        String indirizzo = address.getText().toString();
+        if (indirizzo.isEmpty()) {
+            address.setError("Name Required");
+            address.requestFocus();
+            return;
+        }
+
+        String numero = phone.getText().toString();
+        if (numero.isEmpty()) {
+            phone.setError("Phone Number required");
+            phone.requestFocus();
+            return;
+        }
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("telefono", numero);
+        user.put("username",nick);
+        user.put("indirizzo",indirizzo);
 
 
+        db.collection("utenti")
+                .document(Objects.requireNonNull(mAuth.getCurrentUser().getUid()))
+                .set(user,SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(),"correctly committed data to server",Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "DocumentSnapshot" );
+                        Intent intent =new Intent(ModifyUserInfo.this, mainDashboard.class);
+                        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
 
 
+    @Override
+    public void onClick(View view) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
     //TODO
 
-   /* private void openImageChooser() {
+  /* private void openImageChooser() {
         Dexter.withContext(getApplicationContext())
                 .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
@@ -147,24 +231,24 @@ public class ModifyUserInfo extends AppCompatActivity implements View.OnClickLis
             }
         }).check();
 
-    }*/
+    }
 
 
-    /*private void pickImage() {
+    private void pickImage() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, CHOOSE_IMAGE);
 
-    }*/
+    }
 
-  /*  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CHOOSE_IMAGE && requestCode == RESULT_OK)
             FilePropic = data.getData();
         imageView.setImageURI(FilePropic);
-    }
+    }*/
 
-    private void uploadImageToServer() {
+    /*private void uploadImageToServer() {
         String uniqueId = UUID.randomUUID().toString();
         profileImageRef = FirebaseStorage.getInstance().getReference("profilepic/" + uniqueId);
         UploadTask uploadTask = profileImageRef.putFile(FilePropic);
@@ -201,60 +285,8 @@ public class ModifyUserInfo extends AppCompatActivity implements View.OnClickLis
 
     }*/
 
-    private void saveUserInfo() {
-        String nick = username.getText().toString();
-        if (nick.isEmpty()) {
-            username.setError("Name Required");
-            username.requestFocus();
-            return;
-        }
-
-        String indirizzo = address.getText().toString();
-        if (indirizzo.isEmpty()) {
-            address.setError("Name Required");
-            address.requestFocus();
-            return;
-        }
-
-        String numero = phone.getText().toString();
-        if (numero.isEmpty()) {
-            phone.setError("Phone Number required");
-            phone.requestFocus();
-            return;
-        }
-
-        Map<String, Object> user = new HashMap<>();
-        user.put("telefono", numero);
-        user.put("username",nick);
-        user.put("indirizzo",indirizzo);
-
-
-        db.collection("utenti")
-                .document(Objects.requireNonNull(mAuth.getCurrentUser().getUid()))
-                .set(user,SetOptions.merge())
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(getApplicationContext(),"correctly committed data to server",Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "DocumentSnapshot" );
-                Intent intent =new Intent(ModifyUserInfo.this, mainDashboard.class);
-                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-
-    }
 
 
 
-    @Override
-    public void onClick(View view) {
 
-    }
 }
