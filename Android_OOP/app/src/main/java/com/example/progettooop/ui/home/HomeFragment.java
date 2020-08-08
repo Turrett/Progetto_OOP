579759eaf143f92e9f7d5114726ebc8870d7e821
giovannaferraro <68597201+giovannaferraro@users.noreply.github.com>
2020-08-08 19:13:18 +0200

    migliorata grafica home

--- Android_OOP/app/src/main/java/com/example/progettooop/ui/home/HomeFragment.java
+++ Android_OOP/app/src/main/java/com/example/progettooop/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.progettooop.ui.home;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,18 +16,24 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.progettooop.R;
 import com.example.progettooop.ui.advertisement.Product;
 import com.example.progettooop.ui.search.MyAdapter;
+import com.example.progettooop.ui.user.UserFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
@@ -32,13 +41,15 @@ public class HomeFragment extends Fragment {
     private RecyclerView recyclerView;
     private FirebaseAuth auth;
     private FirebaseFirestore db;
+    private TextView txtloggeduser;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         ArrayList<Product> products  = new ArrayList<Product>();
-
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        txtloggeduser =(TextView) root.findViewById(R.id.lblnomeutentelog);
         searchProductsToRecycleview(root,products);
+        getInfo();
         return root;
 
     }
@@ -90,5 +101,33 @@ public class HomeFragment extends Fragment {
         });
 
     }
+
+
+    private void getInfo(){
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        DocumentReference doc = db.collection("utenti")
+                .document(Objects.requireNonNull(auth.getCurrentUser()).getUid());
+        doc.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    txtloggeduser.setText(documentSnapshot.getString("username"));
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @SuppressLint("ShowToast")
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(),"Fail Loading Data",Toast.LENGTH_SHORT);
+
+            }
+
+        });
+
+    }
+
+
 }
 
