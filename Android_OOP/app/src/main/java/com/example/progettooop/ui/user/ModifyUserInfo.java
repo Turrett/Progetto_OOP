ce8bca754ecb3ecb15851b0d92540e7ecf32aa06
Turrett <45496975+Turrett@users.noreply.github.com>
2020-08-09 20:08:23 +0200

    fixed images

--- Android_OOP/app/src/main/java/com/example/progettooop/ui/user/ModifyUserInfo.java
+++ Android_OOP/app/src/main/java/com/example/progettooop/ui/user/ModifyUserInfo.java
@@ -31,7 +31,6 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -40,19 +39,11 @@ import java.util.Objects;
 
 public class ModifyUserInfo extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int CHOOSE_IMAGE = 101;
-    private static final int PERMISSION_CODE = 102;
-
     private final static String TAG = "Upload user";
     private ImageView image;
-    private EditText username, address, phone, imageName;
+    private EditText username, address, phone;
     private Button save;
     private ProgressBar loadingBar;
-
-    private final static int mWidth = 30;
-    private final static int mLenght = 30;
-    private ArrayList<String> pathArray; //verrà salvato il percorso dell'immagine
-    private int array_position;
 
     private StorageReference mStorageRef;
     private FirebaseAuth mAuth;
@@ -66,15 +57,13 @@ public class ModifyUserInfo extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_user_information);
-        username = (EditText) findViewById(R.id.modify_username);
-        address = (EditText) findViewById(R.id.modify_address);
-        phone =  (EditText) findViewById(R.id.modify_phone);
-        image = (ImageView) findViewById(R.id.imageView);
-        save = (Button) findViewById(R.id.save_changes_button);
-        loadingBar = (ProgressBar) findViewById(R.id.User_progressbar) ;
+        username =  findViewById(R.id.modify_username);
+        address =  findViewById(R.id.modify_address);
+        phone =   findViewById(R.id.modify_phone);
+        image = findViewById(R.id.imageView);
+        save = findViewById(R.id.save_changes_button);
+        loadingBar = findViewById(R.id.User_progressbar) ;
         loadingBar.setVisibility(View.GONE);
-
-        pathArray = new ArrayList<>();
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -154,7 +143,7 @@ public class ModifyUserInfo extends AppCompatActivity implements View.OnClickLis
             UploadTask uploadTask =  ref.putFile(imguri);
             Task<Uri>  uriTask = uploadTask.continueWithTask((task) -> {
                 if (!task.isSuccessful()) {
-                    throw task.getException();
+                    throw Objects.requireNonNull(task.getException());
                 }
                 return ref.getDownloadUrl();
                     }).addOnCompleteListener((task) -> {
@@ -190,13 +179,11 @@ public class ModifyUserInfo extends AppCompatActivity implements View.OnClickLis
                     public void onFailure(@NonNull Exception e) {
                         Log.w(TAG, "Error adding document", e);
                         loadingBar.setVisibility(View.GONE);
-                        return;
                     }
                 });
     }
         else {
             Toast.makeText(getApplicationContext(), "insert a Profile Image", Toast.LENGTH_SHORT).show();
-            return;
         }
 
     }
