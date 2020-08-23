838ff99017f18e7ec06f02ff310981b6a3592fa7
giovannaferraro <68597201+giovannaferraro@users.noreply.github.com>
2020-08-23 17:40:14 +0200

    sistemato le card (e cosa non era card ma ora lo è)

--- Android_OOP/app/src/main/java/com/example/progettooop/ui/recensioni/RecensioniActivity.java
+++ Android_OOP/app/src/main/java/com/example/progettooop/ui/recensioni/RecensioniActivity.java
@@ -10,5 +10,7 @@ public class RecensioniActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_recensione);
+        setTitle("Scrivi qui la tua recensione");
+
     }
 }
